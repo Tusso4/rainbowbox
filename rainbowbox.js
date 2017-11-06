bed995ff697bbeb8dbbbf7bd484d1e4cfb9ed7b3
Tusso4 <tusso4pawno@hotmail.com>
2017-11-06 19:18:22 +0100

    Update rainbowbox.js

--- rainbowbox.js
+++ rainbowbox.js
@@ -1,20 +1,25 @@
+var desde = [0, 0, 0];
+var actual = [0,0,0];
+
 var inter;
 var jumps = 0;
 
-function tryRgb2_callback(num, loop=false, elem) {
+
+function rainbowbox_callback(num, loop=false, elem) {
 	if(hastax.length-1 < jumps && loop == true) {
   	jumps = 0;
   }
 	var toNext = [ [ hastax[jumps][0], hastax[jumps][1], hastax[jumps][2] ] ];
 	var toBack = ($(elem).css("background-color")).substring(4, ($(elem).css("background-color")).length-1).replace(/ /g, '').split(',');
- 
-  tryRgb2($(elem), toBack, toNext, loop);
+  
+  console.log(toBack, toNext, jumps, loop);
+  rainbowbox($(elem), toBack, toNext, loop);
 
   jumps++;
 }
 
-function tryRgb2(elem, from=[0, 0, 0], to=[255, 255, 255], loop=false, vel=20) {
-		//Vars
+function rainbowbox(elem, from=[0, 0, 0], to=[255, 255, 255], loop=false, vel=20) {
+	//Vars
     var rgb_now;
     var rgb;
     var count = 0;
@@ -25,13 +30,13 @@ function tryRgb2(elem, from=[0, 0, 0], to=[255, 255, 255], loop=false, vel=20) {
 		if(to[0].constructor === Array) {
     	toStr = to[0].toString().replace(" ", "").split(",");
     }
-		rgb = toStr;
+	rgb = toStr;
     if(toStr.length < 1) {
     	return false;
     }
 
     var fromStr = from.toString().replace(" ", "").split(",");
-		rgb_now = fromStr;
+	rgb_now = fromStr;
     if(fromStr.length < 1) {
     	return false;
     }
@@ -41,7 +46,7 @@ function tryRgb2(elem, from=[0, 0, 0], to=[255, 255, 255], loop=false, vel=20) {
       if(rgb[0] == rgb_now[0] && rgb[1] == rgb_now[1] && rgb[2] == rgb_now[2]) {
         clearInterval(inter);
         if(to[0].constructor === Array) {
-	        tryRgb2_callback(count, loop, elem);
+	        rainbowbox_callback(count, loop, elem);
         }
       }
       
@@ -55,5 +60,5 @@ function tryRgb2(elem, from=[0, 0, 0], to=[255, 255, 255], loop=false, vel=20) {
       }
 
       $(elem).css("background-color", "rgb("+rgb_now[0]+", "+rgb_now[1]+", "+rgb_now[2]+")");
-		}, vel);
+	}, vel);
 }
